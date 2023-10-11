58ad7986e95cf727db8faf6d70dd9ccf5da8529a
DKalnitak <86296568+DKalnitak@users.noreply.github.com>
2023-10-11 12:47:41 +0530

    Update streamlit_app.py
    stream is kept in comments repeated

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 
-# streamlit.multiselect("pick some fruits:",list(my_fruit_list.index))
+ streamlit.multiselect("pick some fruits:",list(my_fruit_list.index))
 
 # Display the table on the page.
 
@@ -26,7 +26,7 @@ streamlit.dataframe(my_fruit_list)
 
 # lets put a pick list so they can pick the fruit they want to include
 
-streamlit.multiselect("pick some fruits:",list(my_fruit_list.index),['Avocado','Strawberries'])
+# streamlit.multiselect("pick some fruits:",list(my_fruit_list.index),['Avocado','Strawberries'])
 
 fruits_selected=streamlit.multiselect("pick some fruits:",list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show=my_fruit_list.loc[fruits_selected]
